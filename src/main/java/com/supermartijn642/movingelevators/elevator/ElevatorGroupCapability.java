34633270dab9c8c2bb2cdfef896a3de6c5b3bedd
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-11-10 16:53:19 +0100

    Validate controllers are present whenever a chunk is loaded

--- src/main/java/com/supermartijn642/movingelevators/elevator/ElevatorGroupCapability.java
+++ src/main/java/com/supermartijn642/movingelevators/elevator/ElevatorGroupCapability.java
@@ -1,5 +1,7 @@
 package com.supermartijn642.movingelevators.elevator;
 
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
 import com.supermartijn642.movingelevators.MovingElevators;
 import com.supermartijn642.movingelevators.blocks.ControllerBlockEntity;
 import com.supermartijn642.movingelevators.packets.PacketAddElevatorGroup;
@@ -7,21 +9,26 @@ import com.supermartijn642.movingelevators.packets.PacketRemoveElevatorGroup;
 import com.supermartijn642.movingelevators.packets.PacketUpdateElevatorGroups;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.SectionPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraftforge.common.capabilities.*;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.world.ChunkEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -98,15 +105,18 @@ public class ElevatorGroupCapability {
         );
     }
 
+    @SubscribeEvent
+    public static void onLoadChunk(ChunkEvent.Load e){
+        if(e.getWorld() instanceof Level)
+            get((Level)e.getWorld()).validateGroupsInChunk(e.getChunk());
+    }
+
     private final Level level;
     private final Map<ElevatorGroupPosition,ElevatorGroup> groups = new HashMap<>();
+    private final Multimap<ChunkPos,ElevatorGroup> groupsPerChunk = MultimapBuilder.hashKeys().hashSetValues(1).build();
 
     public ElevatorGroupCapability(Level level){
         this.level = level;
-    }
-
-    public ElevatorGroupCapability(){
-        this.level = null;
     }
 
     public ElevatorGroup get(int x, int z, Direction facing){
@@ -115,18 +125,17 @@ public class ElevatorGroupCapability {
 
     public void add(ControllerBlockEntity controller){
         ElevatorGroupPosition pos = new ElevatorGroupPosition(controller.getBlockPos(), controller.getFacing());
-        this.groups.putIfAbsent(pos, new ElevatorGroup(this.level, pos.x, pos.z, pos.facing));
-        this.groups.get(pos).add(controller);
+        ElevatorGroup group = this.groups.computeIfAbsent(pos, p -> new ElevatorGroup(this.level, p.x, p.z, p.facing));
+        this.groupsPerChunk.put(pos.chunkPos(), group);
+        group.add(controller);
     }
 
     public void remove(ControllerBlockEntity controller){
         ElevatorGroupPosition pos = new ElevatorGroupPosition(controller.getBlockPos(), controller.getFacing());
         ElevatorGroup group = this.groups.get(pos);
         group.remove(controller);
-        if(group.getFloorCount() == 0){
-            this.groups.remove(pos);
-            MovingElevators.CHANNEL.sendToDimension(this.level.dimension(), new PacketRemoveElevatorGroup(group));
-        }
+        if(group.getFloorCount() == 0)
+            this.removeGroup(pos);
     }
 
     public void tick(){
@@ -139,12 +148,21 @@ public class ElevatorGroupCapability {
             MovingElevators.CHANNEL.sendToDimension(this.level.dimension(), new PacketAddElevatorGroup(this.writeGroup(group)));
     }
 
+    private void removeGroup(ElevatorGroupPosition pos){
+        ElevatorGroup group = this.groups.remove(pos);
+        if(group != null){
+            this.groupsPerChunk.remove(pos.chunkPos(), group);
+            if(!this.level.isClientSide)
+                MovingElevators.CHANNEL.sendToDimension(this.level.dimension(), new PacketRemoveElevatorGroup(group));
+        }
+    }
+
     /**
      * This should only be called client-side from the {@link PacketRemoveElevatorGroup}
      */
     public void removeGroup(int x, int z, Direction facing){
         if(this.level.isClientSide)
-            this.groups.remove(new ElevatorGroupPosition(x, z, facing));
+            this.removeGroup(new ElevatorGroupPosition(x, z, facing));
     }
 
     public ElevatorGroup getGroup(ControllerBlockEntity entity){
@@ -153,6 +171,16 @@ public class ElevatorGroupCapability {
 
     public Collection<ElevatorGroup> getGroups(){
         return this.groups.values();
+    }
+
+    public void validateGroupsInChunk(ChunkAccess chunk){
+        if(!this.groupsPerChunk.containsKey(chunk.getPos()))
+            return;
+        for(ElevatorGroup group : new ArrayList<>(this.groupsPerChunk.get(chunk.getPos()))){ // Groups may be removed during validation, hence create a copy of the list
+            group.validateControllersExist(chunk);
+            if(group.getFloorCount() == 0)
+                this.removeGroup(new ElevatorGroupPosition(group.x, group.z, group.facing));
+        }
     }
 
     public CompoundTag write(){
@@ -170,15 +198,8 @@ public class ElevatorGroupCapability {
         if(tag instanceof CompoundTag){
             CompoundTag compound = (CompoundTag)tag;
             this.groups.clear();
-            for(String key : compound.getAllKeys()){
-                CompoundTag groupTag = compound.getCompound(key);
-                if(groupTag.contains("group") && groupTag.contains("pos")){
-                    ElevatorGroupPosition pos = ElevatorGroupPosition.read(groupTag.getCompound("pos"));
-                    ElevatorGroup group = new ElevatorGroup(this.level, pos.x, pos.z, pos.facing);
-                    group.read(groupTag.getCompound("group"));
-                    this.groups.put(pos, group);
-                }
-            }
+            for(String key : compound.getAllKeys())
+                this.readGroup(compound.getCompound(key));
         }
     }
 
@@ -195,6 +216,7 @@ public class ElevatorGroupCapability {
             ElevatorGroup group = new ElevatorGroup(this.level, pos.x, pos.z, pos.facing);
             group.read(tag.getCompound("group"));
             this.groups.put(pos, group);
+            this.groupsPerChunk.put(pos.chunkPos(), group);
         }
     }
 
@@ -213,23 +235,27 @@ public class ElevatorGroupCapability {
             this(pos.getX(), pos.getZ(), facing);
         }
 
+        public ChunkPos chunkPos(){
+            return new ChunkPos(SectionPos.blockToSectionCoord(this.x), SectionPos.blockToSectionCoord(this.z));
+        }
+
         @Override
         public boolean equals(Object o){
             if(this == o) return true;
-            if(o == null || getClass() != o.getClass()) return false;
+            if(o == null || this.getClass() != o.getClass()) return false;
 
             ElevatorGroupPosition that = (ElevatorGroupPosition)o;
 
-            if(x != that.x) return false;
-            if(z != that.z) return false;
-            return facing == that.facing;
+            if(this.x != that.x) return false;
+            if(this.z != that.z) return false;
+            return this.facing == that.facing;
         }
 
         @Override
         public int hashCode(){
-            int result = x;
-            result = 31 * result + z;
-            result = 31 * result + (facing != null ? facing.hashCode() : 0);
+            int result = this.x;
+            result = 31 * result + this.z;
+            result = 31 * result + (this.facing != null ? this.facing.hashCode() : 0);
             return result;
         }
 
